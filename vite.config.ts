80374b53508dfe4ebfeceeb7e2acd31ce765aa99
Guido Tapia <guido@ravn.co>
2024-08-06 11:57:52 -0500

    build: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "MantineTailwindAdapter",
+      fileName: "mantine-tailwind-adapter",
+    },
+  },
+});
